fc859d6ef69becd8bfb7ac5e7bbd505e3872f0f7
Donald Wu <yeukfei02@gmail.com>
2024-06-06 13:46:49 +0800

    fix: fix apigateway code

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -21,7 +21,7 @@ resource "aws_api_gateway_resource" "root" {
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   resource_id   = aws_api_gateway_resource.root.id
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
