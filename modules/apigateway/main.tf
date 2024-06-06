1211b79fc179486b86367a628942cee316891c4e
Donald Wu <yeukfei02@gmail.com>
2024-06-06 13:54:18 +0800

    fix: fix apigateway code

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -86,6 +86,5 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_function_name
   principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
 }
